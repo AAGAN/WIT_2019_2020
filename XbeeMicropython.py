3a16a957c1508df96d59825e4a9bc1b71b1e1613
Arash Agan <AAGAN@users.noreply.github.com>
2019-12-25 23:46:11 -0500

    Update XbeeMicropython.py
    the code on the end device only needs to 
    * turn on the pump
    * turn off the pump
    * run one cycle 
    to run one cycle the coordinator should send the acceptable cycle time in milliseconds and acceptable fluctuations to the end device
    on the coordinator, the user should run some sample cycles and check the graph of data and decide the correct values for cycle time and fluctuations before committing to 20000 cycle test

--- XbeeMicropython.py
+++ XbeeMicropython.py
@@ -1,6 +1,7 @@
 import xbee
 from machine import ADC,Pin
 import time
+from math import fabs
 
 temperaturePin = ADC('D0')
 pressurePin = ADC('D1')
@@ -10,13 +11,15 @@ solenoidPin = Pin("D4",Pin.OUT,value=0)
 
 witEndDevice   = b'\x00\x13\xa2\x00\x41\x98\x49\xde'
 witCoordinator = b'\x00\x13\xa2\x00\x41\x97\xff\x02' #or just 0 would work too
+response = ""
 
 class pump:
 	def __init__(self):
+		pumpPinOn.value(0)
 		pumpPinOff.value(1)
 		time.sleep_ms(200)
-		pumpPinOn.value(0)
-
+		pumpPinOff.value(0)
+		
 	def On(self):
 		pumpPinOff.value(0)
 		pumpPinOn.value(1)
@@ -54,8 +57,9 @@ class thermistor:
 		return temperaturePin.read()
 
 #discard all the packets waiting to be read
-while xbee.receive():
-	discard = xbee.receive()
+def discard():
+	while xbee.receive():
+		pass
 
 #define all the objects
 Pump = pump()
@@ -63,39 +67,54 @@ P = pressureTransducer()
 T = thermistor()
 sol = solenoid()
 
-def read():
-	while True:
+def runCycle(cycleTime, fluctuations):#this function needs to be fixed
+	numberOfPoint = 48 #we should determine a safe value for the number of point we can send in one package
+	dt = cycleTime//numOfPoint
+	response.append(str(T.read())+",")
+	sol.pressurize()
+	start = time.ticks_ms()
+	last5 = [0,0,0,0,0]
+	i = 0
+	currentP = P.read()
+	while (time.ticks_diff(time.ticks_ms(),start)<cycleTime and fabs(currentP - last5.mean())>fluctuations):
+		response.append(str(currentP)+",")
+		last5[i]=currentP
+		time.sleep_ms(dt)
+		i += 1
+		if i == 5:
+			i = 0
+		currentP = P.read()
+	sol.depressurize()
+	while (time.ticks_diff(time.ticks_ms(),start)<cycleTime):
+		response.append(str(P.read())+",")
+		time.sleep_ms(dt)
+	response.append(str(T.read()))
+	
+def sendResponse():
+	try:
+		xbee.transmit(0, response) # send to the coordinator
+		print("Data sent successfully")
+	except Exception as e:
+		print("Transmit failure: %s" % str(e))
+			
+While True:
+	try:
 		p = xbee.receive()
 		if p:
-			print(p['payload'])
-			start = time.ticks_ms()
-			payloadT = str(T.read())
-			payloadP = str(P.read())
-			payload = p['payload'].decode('UTF-8')
-			#print(payload1)
-			if payload == 'T':
-				sol.pressurize()
-				xbee.transmit(0, payloadT  + '\r\n') #transmit temperature to the coordinator
-				time.sleep_ms(100)
-				sol.depressurize()
-			if payload == 'P':
-				sol.pressurize()
-				xbee.transmit(0, payloadP + '\r\n')
-				time.sleep_ms(100)
-				sol.depressurize()
-			print('time difference = ' + str(time.ticks_diff(time.ticks_ms(), start)) + ' milliseconds.')
-
-While True:
-	p = xbee.receive()
-	if p:
-		response = ""
-		if p['payload'] == "I":
-			Pump.On()
-			response = "Pump turned on"
-		elif p['payload'] == "O":
-			Pump.Off()
-			response = "Pump turned off"
-		elif "C" in p['payload']:
-			#figure out the set pressure and other info in the message
-			runCycle()
-		sendResponse() #sent the response back
+			print(p['payload']
+			response = ""
+			if p['payload'] == "I":
+				Pump.On()
+				response = "Pump turned on"
+			elif p['payload'] == "O":
+				Pump.Off()
+				response = "Pump turned off"
+			elif "C" in p['payload']: #TODO: needs to be fixed
+				cycleTime = int(p['payload']) #TODO: this needs to be fixed
+			      	fluctuations = int(p['payload']) #TODO: this needs to be fixed
+				runCycle(cycleTime, fluctuations)
+			sendResponse() #sent the response back
+			discard()
+	except Exeption as e:
+		print(str(e))
+			     
